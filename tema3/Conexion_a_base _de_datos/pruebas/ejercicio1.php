5582c8d43cbb20cfc3858d9488f9da09dc80e7d1
Silvia Mesa Cofrades <120189826+silviamescof@users.noreply.github.com>
2023-11-07 20:46:59 +0100

    07/11/2023

---
+++ tema3/Conexion_a_base _de_datos/pruebas/ejercicio1.php
@@ -0,0 +1,12 @@
+<?php
+$cadena_conexion=´mysql:dbname=empresa;host=127.0.0.1´;
+$usuario=´root´;
+$clave=´´;
+try{
+    $bd=new PDO($cadena_conexion,$usuario,$clave);
+    $bd->close();
+
+}catch(PDOException $e){
+    echo ´Error con la base de datos: ´.$e->getMessage();
+}
+?>
