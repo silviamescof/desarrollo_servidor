ffaa960eecb9aa939de430e6a563f9ba36006f00
Silvia Mesa Cofrades <120189826+silviamescof@users.noreply.github.com>
2023-11-26 20:07:26 +0100

    2007

--- tema3/ud4_gestor_restaurante/includes/conexion_bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $cadena_conexion = 'mysql:dbname=pedidos;host=127.0.0.1';
-    $usuario = 'root';
-    $clave = '';
-    $pdo = new PDO($cadena_conexion, $usuario, $clave);
-    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-?>
--- tema3/ud4_gestor_restaurante/inicio/auh.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-/**En este fichero no tiene sentido poner si existe seion porque solo contiene un redireccionamiento */
-
-header("Location:index.php");
-?>
